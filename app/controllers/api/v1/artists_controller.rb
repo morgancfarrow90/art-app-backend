63dd7fdc4c4f2a34df7076e99e3ec7b3bdec8234
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-07-23 09:43:11 -0400

    implemented artist and artwork serializer

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::ArtistsController < ApplicationController
+
+  def index
+    artists = Artist.all
+    render json: ArtistSerializer.new(artists)
+  end
+
+  def create
+    artist= Artist.new(artist_params)
+    if artist.save
+      render json: artist, status: :accepted
+    else
+      render json: {errors: artist.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def artist_params
+    params.require(artist).permit(:name, :biography)
+  end
+
 end
